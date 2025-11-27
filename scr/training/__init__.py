37733b3aca42bcad17d11be1756cbd0aabae7b70
Emaagr <142398826+Emaagr@users.noreply.github.com>
2025-11-27 18:00:33 +0100

    Refactor imports from models_common to training_utils

--- scr/training/__init__.py
+++ scr/training/__init__.py
@@ -3,5 +3,5 @@
 # Initialize the training module
 from .train_2class import my_train
 from .train_3class import my_train_3class
-from .models_common import CustomDataset, DeviceDataLoader
-from .models_common import CombinedModel, MLPModule, ModifiedResNet18
+from .training_utils import CustomDataset, DeviceDataLoader
+from .training_utils import CombinedModel, MLPModule, ModifiedResNet18
