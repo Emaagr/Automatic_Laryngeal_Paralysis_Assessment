68e76440ea93ec31c72a2e704cefa716eb88bee4
Emaagr <142398826+Emaagr@users.noreply.github.com>
2025-11-27 17:57:47 +0100

    Initialize ablation package for laryngeal paralysis project
    This package groups utilities and scripts for ablation studies on binary and 3-class classification.

---
+++ scr/ablation/__init__.py
@@ -0,0 +1,34 @@
+# src/ablation/__init__.py
+"""
+Ablation package for the laryngeal paralysis project.
+
+This package groups utilities and scripts to run ablation studies on:
+- Binary classification (Normal vs Paralyzed)
+- 3-class classification (Normal / Monolateral / Bilateral)
+
+Modules
+-------
+- ablation_2class   : main script for 2-class ablation.
+- ablation_3class   : main script for 3-class ablation.
+- ablation_utils    : shared utilities (He init, reinit, training loop, Shapley, etc.).
+"""
+
+from . import ablation_2class
+from . import ablation_3class
+from .ablation_utils import (
+    he_init,
+    reinit_weights,
+    select_additional_indices,
+    my_train,
+    shapley_value,
+)
+
+__all__ = [
+    "ablation_2class",
+    "ablation_3class",
+    "he_init",
+    "reinit_weights",
+    "select_additional_indices",
+    "my_train",
+    "shapley_value",
+]
