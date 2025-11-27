a2c57f4b6c959c07aca013ad35733f9cb95eba65
Emaagr <142398826+Emaagr@users.noreply.github.com>
2025-11-27 18:30:17 +0100

    Update __init__.py

--- scr/feature_extraction/__init__.py
+++ scr/feature_extraction/__init__.py
@@ -1,6 +1,5 @@
 # src/feature_extraction/__init__.py
 
-# Modulo per estrazione caratteristiche (Feature Extraction)
-# Puoi lasciare vuoto, oppure aggiungere import di funzioni/utilità globali
+# Modules for Feature Extraction
 from .feature_extraction import feature_extract, ggm
 from .utils import ensure_dir, filter_outliers, extract_frame
